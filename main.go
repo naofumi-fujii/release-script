4277e206fd335726fc4900fd98590a1b818a1210
naofumi-fujii <naofumi.fujii@gmail.com>
2018-03-08 12:53:03 +0900

    .

--- main.go
+++ main.go
@@ -7,8 +7,22 @@ import (
 )
 
 func main() {
+	branchName := fmt.Sprintf("%s", os.Args[1])
+
 	// git branch -D $(BRANCH_NAME)
+	out, err := exec.Command("git", "branch", "-D", branchName).Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+
 	// git checkout develop
+	out, err = exec.Command("git", "checkout", "develop").Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+
 	// git pull
 	// git checkout -b $(BRANCH_NAME)
 	// git push --set-upstream origin $(BRANCH_NAME)
@@ -17,10 +31,4 @@ func main() {
 	// git commit -m '$(COMMIT_MSG)'
 	// git push
 
-	branchName := fmt.Sprintf("%s", os.Args[1])
-	out, err := exec.Command("git", "checkout", "-b", branchName).Output()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(out))
 }
